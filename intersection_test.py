57013da9c9ea88fd62c8284f2d220c61c34f71b1
Yueqi "Nicole" Li <102627863+NicoleYueqiLi@users.noreply.github.com>
2022-08-01 15:34:35 -0400

    Update intersection_test.py

--- intersection_test.py
+++ intersection_test.py
@@ -41,18 +41,18 @@ def timing_test(input_file_name, output_file_name):
     test = load_test_file(input_file_name)
     total_time = 0
     with open(output_file_name, 'w') as fp:
-      for i in range(len(test)-1):
-          s1 = RealSet(*eval(test[i]))
-          s2 = RealSet(*eval(test[i + 1]))
-          start = process_time()
-          s1.intersection(s2)
-          end = process_time()
-          tim = end - start
-          print(tim, end='\r')
-          total_time += tim
-          fp.write("%s\n" % tim)
-    fp.write("%s\n" % "Average:")
-    avg = total_time / (len(test) - 1)
-    fp.write("%s\n" % avg)
+        for i in range(len(test)-1):
+            s1 = RealSet(*eval(test[i]))
+            s2 = RealSet(*eval(test[i + 1]))
+            start = process_time()
+            s1.intersection(s2)
+            end = process_time()
+            tim = end - start
+            print(tim, end='\r')
+            total_time += tim
+            fp.write("%s\n" % tim)
+        fp.write("%s\n" % "Average:")
+        avg = total_time / (len(test) - 1)
+        fp.write("%s\n" % avg)
     print("average", avg)
     print("done")
