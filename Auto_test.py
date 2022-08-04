6d53543cdefc5df1197b932d319e6c6d56be99ba
Yueqi "Nicole" Li <102627863+NicoleYueqiLi@users.noreply.github.com>
2022-08-03 21:40:35 -0400

    Update Auto_test.py

--- Auto_test.py
+++ Auto_test.py
@@ -20,14 +20,16 @@ def load_test_file(input_file_name):
         lines = rd.readlines()
         return lines
 
+    
 
-def timing_test(input_file_name):
+
+def timing_test():
     """
-    Testing intersection cpu time
+    Testing union, intersection, difference, symmetric_difference, and is_disjoint cpu time
+    
     INPUT: 
     
     - ``input_file_name``: testing file name
-    - ``output_file_name``: output file name, where out stored
     
     OUTPUT:
     
@@ -37,14 +39,33 @@ def timing_test(input_file_name):
     
         timing_test('difference_test.txt', 'difference_res.txt')
     """
-    test = load_test_file(input_file_name)
-    total_time = 0
+    test_file = ["test_union.txt", "intersection_test.txt","test_difference.txt","test_symmetric_difference.txt","test_is_disjoint.txt"]
     test_contents = {"union": 0, 
                      "intersection": 0, 
                      "difference": 0,
                      "symmetric_difference":0,
                      "is_disjoint":0}
-    def test_inner(s1, s2, test_name):
+    total_time = 0
+    for test_name in test_contents:
+        for test in test_file:
+            test = load_test_file(test)
+            s1, s2 = None, RealSet(*eval(test[0]))
+            for i in range(len(test)-1):
+            s1, s2 = s2, RealSet(*eval(test[i + 1])
+                output_dir = "res_" + test_name + ".txt"
+                tim = test_inner(test_name)
+                print(tim, end='\r')
+                test_contents[test_name] += tim
+                with open(output_dir, 'a') as fp:
+                    fp.write("%s\n" % tim)
+     for test_name in test_contents:
+        output_dir = test_name + "_res.txt"
+        with open(output_dir, 'a') as fp:
+            fp.write("Average: {avg:.6f}".format(avg=test_contents[test_name] / (len(test) - 1)))
+        print("{test} Average Time: {avg:.6f}".format(test=test_name, avg=test_contents[test_name] / (len(test) - 1)))
+    print("done") 
+        
+    def test_inner(s1, s2):
         if test_name == "union":
             start = process_time()
             s1.union(s2)
@@ -68,21 +89,21 @@ def timing_test(input_file_name):
         else:
             raise ValueError("Invalid testing name: " + test_name)
         return end-start
-    s1, s2 = None, RealSet(*eval(test[0]))
-    for i in range(len(test)-1):
-        s1, s2 = s2, RealSet(*eval(test[i + 1]))
-        for test_name in test_contents:
-            output_dir = "res_" + test_name + ".txt"
-            tim = test_inner(s1, s2, test_name)
-            print(tim, end='\r')
-            test_contents[test_name] += tim
-            with open(output_dir, 'a') as fp:
-                fp.write("%s\n" % tim)
+#     s1, s2 = None, RealSet(*eval(test[0]))
+#     for i in range(len(test)-1):
+#         s1, s2 = s2, RealSet(*eval(test[i + 1]))
+#         for test_name in test_contents:
+#             output_dir = "res_" + test_name + ".txt"
+#             tim = test_inner(s1, s2, test_name)
+#             print(tim, end='\r')
+#             test_contents[test_name] += tim
+#             with open(output_dir, 'a') as fp:
+#                 fp.write("%s\n" % tim)
     
-    for test_name in test_contents:
-        output_dir = test_name + "_res.txt"
-        with open(output_dir, 'a') as fp:
-            fp.write("Average: {avg:.6f}".format(avg=test_contents[test_name] / (len(test) - 1)))
-        print("{test} Average Time: {avg:.6f}".format(test=test_name, avg=test_contents[test_name] / (len(test) - 1)))
-    print("done") 
+#     for test_name in test_contents:
+#         output_dir = test_name + "_res.txt"
+#         with open(output_dir, 'a') as fp:
+#             fp.write("Average: {avg:.6f}".format(avg=test_contents[test_name] / (len(test) - 1)))
+#         print("{test} Average Time: {avg:.6f}".format(test=test_name, avg=test_contents[test_name] / (len(test) - 1)))
+#     print("done") 
     
